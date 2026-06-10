1e3f2afd5e28daf6af980b98ea1c0fc776eca817
tuan123123 <125374828+tuan123123@users.noreply.github.com>
2026-06-09 22:53:18 -0500

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        #Monotonic Stack
+        temps = temperatures
+        n = len(temps)
+        answer = [0] * n
+        stk = []
+
+        for i, t in enumerate(temps):
+            while stk and stk[-1][0] < t:
+                stk_t, stk_i = stk.pop()
+                answer[stk_i] = i - stk_i
+            
+            stk.append((t, i))
+        
+        return answer
