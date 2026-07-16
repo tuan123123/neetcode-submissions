a91a26cea772271bff7eaced4ec86946585cce5c
tuan123123 <125374828+tuan123123@users.noreply.github.com>
2026-07-15 20:56:49 -0500

    Add: course-schedule-iv - submission-3

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-3.py
@@ -0,0 +1,37 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        graph = [[] for _ in range(numCourses)]
+
+        indegree = [0] * numCourses
+
+        all_prereqs = [set() for _ in range(numCourses)]
+
+        for u,v in prerequisites:
+            graph[u].append(v)
+            indegree[v] += 1
+
+        q = deque()
+
+        for course in range(numCourses):
+            if indegree[course] == 0:
+                q.append(course)
+
+        while q:
+            course = q.popleft()
+
+            for next_course in graph[course]:
+                all_prereqs[next_course].add(course)
+
+                all_prereqs[next_course].update(all_prereqs[course])
+
+                indegree[next_course] -= 1
+
+                if indegree[next_course] == 0:
+                    q.append(next_course)
+
+        result = []
+
+        for u,v in queries:
+            result.append(u in all_prereqs[v])
+
+        return result
