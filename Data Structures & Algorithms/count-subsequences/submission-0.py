fa549081429f48dfdace21ad424942486da8baa6
tuan123123 <125374828+tuan123123@users.noreply.github.com>
2026-07-26 18:12:35 -0500

    Add: count-subsequences - submission-0

---
+++ Data Structures & Algorithms/count-subsequences/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        memo = {}
+        def dfs(i, j):
+            if len(t) == j:
+                return 1
+            if len(s) == i:
+                return 0
+            if (i, j) in memo:
+                return memo[(i, j)]
+            
+            if s[i] == t[j]:
+                memo[(i, j)] = dfs(i + 1, j + 1) + dfs(i + 1, j)
+            
+            else:
+                memo[(i, j)] = dfs(i + 1, j)
+            
+            return memo[(i, j)]
+        
+        return dfs(0, 0)
