d9ba7adf6946df00d4d2c1097d5994209249184c
tuan123123 <125374828+tuan123123@users.noreply.github.com>
2026-08-24 20:11:18 -0700

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        """
+        abbadc
+
+        """
+        def is_palin(l, r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            
+            return True
+        
+        l = 0
+        r =  len(s) - 1
+
+        while l < r:
+            if s[l] != s[r]:
+                return (is_palin(l + 1, r) or is_palin(l, r - 1))
+            l += 1
+            r -= 1
+        
+        return True
+
+
+        
+
+            
+
+        
